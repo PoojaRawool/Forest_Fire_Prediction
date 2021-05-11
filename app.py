c321caaa82c0cc182bf666139fd7535512530fab
PoojaRawool <84032150+PoojaRawool@users.noreply.github.com>
2021-05-11 21:24:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+app = Flask(__name__,template_folder='Templates')
+model = pickle.load(open('Forest_Fire_Prediction.pkl', 'rb'))
+@app.route("/",methods=['GET'])
+@cross_origin()
+def home():
+	return render_template("index.html")
+
+@app.route("/predict",methods=['GET', 'POST'])
+@cross_origin()
+def predict():
+    if request.method == 'POST':
+        # latitude
+        latitude =float(request.form['latitude'])
+        # longitude
+        longitude = float(request.form['longitude'])
+        # brightness
+        brightness = float(request.form['brightness'])
+        # track
+        track = float(request.form['track'])
+        # acq_time
+        acq_time = int(request.form['acq_time'])
+        # confidence
+        confidence = int(request.form['confidence'])
+        # day
+        day = int(request.form['day'])
+        # month
+        month = int(request.form['month'])
+        # year
+        year = int(request.form['year'])
+        daynight =(request.form['daynight'])
+        if(daynight=='day'):
+            daynight=1
+        else:
+            daynight=0	
+        prediction=model.predict([[latitude,longitude,brightness,track,acq_time,confidence,day,month,year,daynight]])
+        output=round(prediction[0],2)
+        if output>0:
+            return render_template('predictor.html',prediction_text="Frp value is  {}".format(output))
+    else:
+        return render_template('predictor.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
